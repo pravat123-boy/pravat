97e0f953cc6dc4debf58e45417ea83452257c276
prava <pravatmohapatra123@gmail.com>
2024-09-29 16:23:14 +0530

    commit

--- PravatModule/src/Pravat_Package/PravatClass.java
+++ PravatModule/src/Pravat_Package/PravatClass.java
@@ -3,7 +3,8 @@ package Pravat_Package;
 public class PravatClass {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Hello and welcome!")   ;
+
     }
 }
 
